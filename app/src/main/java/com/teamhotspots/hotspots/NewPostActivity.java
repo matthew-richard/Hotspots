95c86fd7c9a2667f71b26b2257da0f4345f99127
John Michael Kuhn Jr <kuhn.johnmichael@gmail.com>
2017-04-04 13:30:29 -0400

    Need to add button styles and do photo fragment

--- app/src/main/java/com/teamhotspots/hotspots/NewPostActivity.java
+++ app/src/main/java/com/teamhotspots/hotspots/NewPostActivity.java
@@ -82,25 +82,22 @@ public class NewPostActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class TextFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public TextFragment() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
+        public static TextFragment newInstance() {
+            TextFragment fragment = new TextFragment();
             return fragment;
         }
 
@@ -108,8 +105,33 @@ public class NewPostActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_new_post, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    public static class PhotoFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PhotoFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PhotoFragment newInstance() {
+            PhotoFragment fragment = new PhotoFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_new_post, container, false);
             return rootView;
         }
     }
@@ -128,7 +150,13 @@ public class NewPostActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    return TextFragment.newInstance();
+                case 1:
+                    return PhotoFragment.newInstance();
+            }
+            return TextFragment.newInstance();
         }
 
         @Override
