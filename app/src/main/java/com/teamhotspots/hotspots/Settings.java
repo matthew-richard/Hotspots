4990260c59ae293c55f04d7a7fff788a4161d9d9
diananing <diananing1337@gmail.com>
2017-05-04 21:23:11 -0400

    pref

--- app/src/main/java/com/teamhotspots/hotspots/Settings.java
+++ app/src/main/java/com/teamhotspots/hotspots/Settings.java
@@ -95,7 +95,7 @@ public class Settings extends Fragment {
                     Toast.makeText(getActivity(), "Username cannot be empty",
                             Toast.LENGTH_LONG).show();
                 } else {
-                    SharedPreferences sharedPref = getActivity().getPreferences(MODE_PRIVATE);
+                    SharedPreferences sharedPref = getActivity().getSharedPreferences("PREF", MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPref.edit();
                     editor.putString(getString(R.string.username), et.getText().toString());
                     editor.commit();
@@ -105,7 +105,7 @@ public class Settings extends Fragment {
                     filepath.putFile(mPhotoUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            SharedPreferences sharedPref = getActivity().getPreferences(MODE_PRIVATE);
+                            SharedPreferences sharedPref = getActivity().getSharedPreferences("PREF", MODE_PRIVATE);
                             SharedPreferences.Editor editor = sharedPref.edit();
                             String imageUrl = taskSnapshot.getDownloadUrl().toString();
                             editor.putString("ICON_PATH", imageUrl);
